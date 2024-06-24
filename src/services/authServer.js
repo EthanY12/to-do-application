898174867d47b6f90b5ea2470346901b4d7e6d26
Ethan Young <ethany777@hotmail.com>
2024-06-24 14:08:00 +0100

    Login and register pretty

--- src/services/authServer.js
+++ src/services/authServer.js
@@ -1,32 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:3000';
+const API_URL = "http://localhost:5000";
 
 const register = (username, password) => {
-    return axios.post(`${API_URL}/register`, { username, password });
+  return axios.post(`${API_URL}/register`, { username, password });
 };
 
 const login = (username, password) => {
-    return axios.post(`${API_URL}/login`, { username, password })
-    .then(response => {
-        if (response.data.token) {
-        localStorage.setItem('user', JSON.stringify(response.data));
-        }
-        return response.data;
+  return axios
+    .post(`${API_URL}/login`, { username, password })
+    .then((response) => {
+      if (response.data.token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
     });
 };
 
 const logout = () => {
-    localStorage.removeItem('user');
+  localStorage.removeItem("user");
 };
 
 const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem('user'));
+  return JSON.parse(localStorage.getItem("user"));
 };
 
 export default {
   register,
   login,
   logout,
-  getCurrentUser
+  getCurrentUser,
 };
