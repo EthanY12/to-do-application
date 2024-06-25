d2d9cd03ae24924d1e4d7f43af46d6d82aff5380
Ethan Young <ethany777@hotmail.com>
2024-06-25 15:17:00 +0100

    KAY-17 Builds calender end point

---
+++ src/components/Calender.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Calendar = ({ user }) => {
+  return (
+    <div>
+      <h2>Welcome to the Calendar, {user.username}!</h2>
+      <div>
+        {/* Calendar implementation goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
--- src/services/authServer.js
+++ src/services/authServer.js
@@ -1,33 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = "http://localhost:5000";
+const API_URL = 'http://localhost:5000';
 
 const register = (username, password) => {
   return axios.post(`${API_URL}/register`, { username, password });
 };
 
-const login = (username, password) => {
-  return axios
-    .post(`${API_URL}/login`, { username, password })
-    .then((response) => {
-      if (response.data.token) {
-        localStorage.setItem("user", JSON.stringify(response.data));
-      }
-      return response.data;
-    });
+const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  if (response.data.token) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
 };
 
 const logout = () => {
-  localStorage.removeItem("user");
+  localStorage.removeItem('user');
 };
 
 const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem("user"));
+  return JSON.parse(localStorage.getItem('user'));
 };
 
 export default {
   register,
   login,
   logout,
-  getCurrentUser,
+  getCurrentUser
 };
